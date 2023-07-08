68b2be72960c6964348168ff33a71a4135ee8cf4
notfinancialadvice <notfinancialadvice@notfinavicesMBP.lan>
2023-07-08 03:17:02 -0500

    adding privacy policy

--- src/pages/2.js
+++ src/pages/2.js
@@ -1,61 +1,12 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import fs from 'fs';
+import path from 'path';
 
-// This page will show up at the route /mypage
+export default function MyPage({ html }) {
+  return <div class="bg-white" dangerouslySetInnerHTML={{ __html: html }} />;
+}
 
-import {
-  PlasmicRootProvider,
-  PlasmicComponent,
-  ComponentRenderData,
-  extractPlasmicQueryData
-} from '@plasmicapp/loader-nextjs';
-import { useRouter } from 'next/router';
-import { PLASMIC } from '../../plasmic-init';
-
-const inter = Inter({ subsets: ['latin'] });
-
-// Statically fetch the data needed to render Plasmic pages or components.
-export const getStaticProps = async () => {
-  // You can pass in multiple page paths or component names.
-  const plasmicData = await PLASMIC.fetchComponentData('Home');
-  if (!plasmicData) {
-    throw new Error('No Plasmic design found');
-  }
-
-  const compMeta = plasmicData.entryCompMetas[0];
-
-  // Cache the necessary data fetched for the page
-  const queryCache = await extractPlasmicQueryData(
-    <PlasmicRootProvider loader={PLASMIC} prefetchedData={plasmicData} pageParams={compMeta.params}>
-      <PlasmicComponent component={compMeta.displayName} />
-    </PlasmicRootProvider>
-  );
-  return {
-    props: {
-      plasmicData,
-      queryCache
-      // ...
-    },
-
-    // Using incremental static regeneration, will invalidate this page
-    // after 300s (no deploy webhooks needed)
-    revalidate: 300
-  };
-};
-
-// Render the page or component from Plasmic.
-export default function MyPage(props) {
-  const router = useRouter();
-  const compMeta = props.plasmicData.entryCompMetas[0];
-  return (
-    <PlasmicRootProvider
-      loader={PLASMIC}
-      // prefetchedData={props.plasmicData}
-      // prefetchedQueryData={props.queryCache}
-      // pageParams={compMeta.params}
-      // pageQuery={router.query}
-    >
-      <PlasmicComponent component='Home' />
-    </PlasmicRootProvider>
-  );
-};
+export async function getStaticProps() {
+  const filePath = path.join(process.cwd(), 'privacy.html');
+  const html = fs.readFileSync(filePath, 'utf8');
+  return { props: { html } };
+}
